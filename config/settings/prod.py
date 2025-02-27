9d996458bae04dd699f09115243f9bdb86047f23
Jaeho Yoo <mung5596y@gmail.com>
2025-02-28 00:33:47 +0800

    asdfasdfadfdsadf

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['18.140.191.79', 'bizzlerclub.com']
+ALLOWED_HOSTS = ['18.140.191.79', 'bizzlerclub.com', 'www.bizzlerclub.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
